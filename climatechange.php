9ce1e1997b7c58a1b14ff9e4f7f92f91e07a7bf6
marco <m.baroncini@cyberway.cloud>
2022-10-26 11:18:23 +0200

    Initial commit

---
+++ climatechange.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package ClimateChange
+ */
+/*
+Plugin Name: ClimateChange data
+Description: Display https://global-warming.org/ data
+Version: 0.1.0
+Requires at least: 5.0
+Requires PHP: ^7.4
+Author: Cyberway
+License: GPLv2 or later
+Text Domain: climatechange
+*/
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+define( 'CLIMATECHANGE_VERSION', '5.0.1' );
+define( 'CLIMATECHANGE__MINIMUM_WP_VERSION', '5.0' );
+define( 'CLIMATECHANGE__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+
+//register_activation_hook( __FILE__, array( 'Climatechange', 'plugin_activation' ) );
+//register_deactivation_hook( __FILE__, array( 'Climatechange', 'plugin_deactivation' ) );
